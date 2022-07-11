7226f85e29e0aed8081e97602ac1daf4627033c2
Smith <joshua.smith@mail.campbelloil.com>
2022-07-10 22:32:50 -0400

    Updated ex12.py

--- ex12.py
+++ ex12.py
@@ -1,5 +1,8 @@
+#Assigns input statement value to variable
 age= input("How old are you? ")
+#Assigns input statement value to variable
 height = input("How tall are you? ")
+#ASsigns input statement value to variable
 weight = input("How much do you weigh? ")
-
+#Prnts 'so you're $age old, $height tall and $weight heavy' 
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
