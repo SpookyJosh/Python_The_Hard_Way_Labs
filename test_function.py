de41d3fd17018feb4418f7138ffd4cb9567350bb
Smith <joshua.smith@mail.Campbelloil.com>
2022-07-23 15:40:20 -0400

    Moar thangs with functions

---
+++ test_function.py
@@ -0,0 +1,12 @@
+def skittles(flavor1, flavor2, flavor3):
+    print(f"We have the following flavors: {flavor1}, {flavor2}, and {flavor3}")
+
+#first = input("Enter first flavor: ")
+#second = input("Enter your second flavor: ")
+#third = input("Enter your third flavor: ")
+
+#skittles(first, second, third)
+
+#skittles("red", "green", "yellow")
+
+skittles(input("Enter your first flavor: "), input("Enter your second flavor: "), input("Enter your third flavor: "))
