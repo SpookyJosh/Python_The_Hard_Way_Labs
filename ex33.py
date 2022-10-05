64bfb913b4d64f3658aacbe77379ff166fb9e9e3
Josh Smith <jsmith.wooster@gmail.com>
2022-10-04 22:16:01 -0400

    Some while statements

---
+++ ex33.py
@@ -0,0 +1,8 @@
+numbers=[]
+for i in range(0,6):
+    print(f"i is {i}")
+    numbers.append(i)
+    print("Numbers now:", numbers)
+    print("The numbers: ")
+    for num in numbers:
+        print(num)
