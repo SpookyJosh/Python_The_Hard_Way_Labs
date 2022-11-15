3e89c072aa2aadff8f00982485976f7f79f52497
Josh Smith <jsmith.wooster@gmail.com>
2022-11-14 21:15:00 -0500

    Some more progress

--- ex36.py
+++ ex36.py
@@ -1,25 +1,53 @@
+from sys import exit
+
 def left():
-
-def right():
-
-
-def start():
-    print("You walk into the kitchen")
-    print("Your tummy is grumbling")
-    print("You are so hungry you could eat a horse")
-    print("There are two cupboards, one to the left and one to the right")
-    print("Select either the left or the right cupboard")
-    
+    print("> You reach for the left cupboard")
+    print("> You reach around, you feel a plastic container and a glass jar")
+    print("> Which will you choose? The container or the jar?")
     choice=input("> ")
 
+    if choice=='jar':
+        print("> You get a jar full of raisins")
+        exit(0)
+    elif choice=='container':
+        print("> Congrats, you got a container full of cookies")
+        exit(0)
+    else:
+        none()
+def right():
+    print("> You reach for the right cupboard")
+    print("> You feel a bag and a box, which do you grab?")
+    choice=input("> ")
+
+    if choice=='bag':
+        print("> You got a bag of wheat thins")
+        exit(0)
+    elif choice=='box':
+        print("> congrats you got a bag of scooby doo fruit snacks")
+        exit(0)
+    else:
+        none()
+def none():
+    print("> Sorry bud, you ain't gettin no snacks today")
+
+def start():
+    print("> You walk into the kitchen")
+    print("> Your tummy is grumbling")
+    print("> You are so hungry you could eat a horse")
+    print("> There are two cupboards, one to the left and one to the right")
+    print("> Select either the left or the right cupboard")
+
+    mylist=['right','left','neither']
+    choice=input(f"> Your options are {mylist[0]}, {mylist[1]}, and {mylist[2]}. Make a choice: ")
+
     if choice=='left':
-        print("looks like you chose the left cupboard")
+        print("> looks like you chose the left cupboard")
         left()
 
     elif choice=='right':
-        print("looks like you chose the right cupboard")
+        print("> looks like you chose the right cupboard")
         right()
     else:
-        print("You are directionally challenged, no snack for you")
+        print("> You chose neither, goodbye")
         exit(0)
 start()
